c8d1a9d46dba24a2e2b6244aaa213a6d42cf8d4f
olcayusta <olcayusta02@gmail.com>
2020-10-03 17:26:37 +0300

    user list module redesigned

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -5,13 +5,17 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { UserListItemComponent } from './user-list-item/user-list-item.component';
 import { UserListComponent } from './user-list/user-list.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [UsersComponent, UserListItemComponent, UserListComponent],
-  imports: [
-    CommonModule,
-    UsersRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        UsersRoutingModule,
+        MatDividerModule,
+        MatButtonModule
+    ]
 })
 export class UsersModule { }
