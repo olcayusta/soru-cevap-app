1eb29a4d29c50dbba0fb3a19b84e5484b6d3325a
olcayusta <olcayusta02@gmail.com>
2020-10-13 01:12:14 +0300

    fixed 0112

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,25 +1,23 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
-import {fromEvent, merge, Observable, of} from 'rxjs';
-import {mapTo} from 'rxjs/operators';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { fromEvent, merge, Observable, of } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BannerComponent implements OnInit {
   online$: Observable<boolean>;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
     this.online$ = merge(
       of(navigator.onLine),
       fromEvent(window, 'online').pipe(mapTo(true)),
-      fromEvent(window, 'offline').pipe(mapTo(false)),
+      fromEvent(window, 'offline').pipe(mapTo(false))
     );
   }
-
 }
